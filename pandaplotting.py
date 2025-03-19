bd8db7a76f9c43f730390d8e15426efec8412ac4
nsakwa <nsakwa@gmail.com>
2025-03-20 02:29:39 +0300

    pandas plotting commit

---
+++ pandaplotting.py
@@ -0,0 +1,12 @@
+import sys 
+import matplotlib
+matplotlib.use('Agg')
+
+import pandas as pd
+import matplotlib.pyplot as plt 
+df = pd.read_csv('dataset.csv')
+df.plot()
+plt.show()
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
+
---
+++ read_csv.py
@@ -0,0 +1,3 @@
+import pandas as pd 
+df =pd.read_csv('data.csv')
+print(dt.to_string())
